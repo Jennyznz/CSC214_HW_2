296c1449611a42559877af190e5e6e8e53e3464c
Zheng <jzheng22@ret201-stud-28.ur.rochester.edu>
2024-10-04 14:31:06 -0400

    Initial commit

---
+++ Posters/PostersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostersApp.swift
+//  Posters
+//
+//  Created by Zheng, Jenny on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PostersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
